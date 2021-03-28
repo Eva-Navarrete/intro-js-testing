13294e8c652e8499af5cfb776f89475cd19285bd
Eva <>
2021-03-28 05:20:16 -0600

    Completed intro to js testing practice

--- tweet.js
+++ tweet.js
@@ -1,4 +1,23 @@
 class Tweet {
+  constructor(sender, content) {
+    this.sender = sender;
+    this.content = content;
+    this.likes = 0;
+    this.retweets = 0;
+    this.replies = [];
+    this.replyCount = 0;
+    this.likes = 0;
+  }
+  like() {
+    this.likes ++;
+  }
+  retweet() {
+    this.retweets ++;
+  }
+  reply(reply) {
+    this.replies.push(reply);
+    this.replyCount ++;
+  }
 
 }
 
